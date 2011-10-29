1425160d1b2a29a869b3e39fff6234174ab56e93
Rollen Gomes <avelliance@gmail.com>
2011-10-29 17:04:05 +0800

    removed jasmine default files

--- spec/javascripts/InstaSpec.js
+++ spec/javascripts/InstaSpec.js
@@ -1,3 +1,17 @@
 describe('Insta', function() {
-  loadFixtures('sample.html')
+  beforeEach(function () {
+    $('#jasmine_content').append('<div id="fixture"></div>');
+    $('#fixture').append('<h1 id="target">This is a title</h1>');
+  });
+
+  it('shoud have a target to act on', function() {
+    expect($('#target')).toExist();
+  });
+
+  it('should show a textarea when clicked', function() {
+    $('#target').insta();
+    $('#target').click();
+
+    expect($('div #insta_target')).toExist();
+  });
 });
---
+++ spec/javascripts/helpers/jquery.insta.js
@@ -0,0 +1,5 @@
+(function($){
+  $.fn.insta = function(){
+    target_id = this.attr('id') 
+  };
+})( jQuery );
